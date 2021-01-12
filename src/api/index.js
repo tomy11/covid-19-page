629dcd39011d7caad6934364dd99793c86717e89
DESKTOP-KPINSP-\KP-INSP-01 <prachya60050@hotmail.com>
2021-01-12 21:49:02 +0700

    add api

--- src/api/index.js
+++ src/api/index.js
@@ -1,2 +1,20 @@
 import axios from 'axios';
-const url = 'https://disease.sh/v3/covid-19/historical'
+const url = 'https://disease.sh/v3/covid-19/historical';
+
+export const fetchData = async (total) => {
+    let changeableUrl = url;
+
+    if (total) {
+        changeableUrl = `${url}/?lastdays=${total}`;
+    }
+
+    try {
+        const { data } = await axios.get(changeableUrl);
+        return data;
+    } 
+    catch (error) {
+        return error;
+    }
+    
+
+}
